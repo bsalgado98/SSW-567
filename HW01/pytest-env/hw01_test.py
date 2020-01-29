9625b3f7cbcf29d1b9857d099e2e5bec056e2e3d
Bruno Salgado <bsalgado@stevens.edu>
2020-01-28 20:12:05 -0500

    Completed various tests for classify_triangles function

--- HW01/pytest-env/hw01_test.py
+++ HW01/pytest-env/hw01_test.py
@@ -1,4 +1,11 @@
 import hw01
 
 def test_classify_triangle():
-    assert classify_triangl(3, 4, 5) == 'scalene right'
+    assert hw01.classify_triangle(3, 4, 5) == "scalene right"
+    assert hw01.classify_triangle(10, 10, 10) == "equilateral not right"
+    assert hw01.classify_triangle(10, 10, 9) == "isosceles not right"
+    assert hw01.classify_triangle(0.256, 400, 0.256) == "isosceles not right"
+    assert hw01.classify_triangle(0, -1, -2) == "scalene not right"
+    assert hw01.classify_triangle(-21, -28, -35) == "scalene right"
+    assert hw01.classify_triangle(3.99999999, 4.99999999, 5.99999999) == "scalene not right"
+    assert hw01.classify_triangle(2.99999999, 3.99999999, 4.99999999) == "scalene not right"
