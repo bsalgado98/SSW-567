c4201717be677d6327c1733c0e1ddff759335bd7
Bruno Salgado <bsalgado@stevens.edu>
2020-02-04 11:38:12 -0500

    Added custom test cases to TestTriangle

--- HW02/pytest-env/TestTriangle.py
+++ HW02/pytest-env/TestTriangle.py
@@ -18,13 +18,30 @@ class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        self.assertEqual(classifyTriangle(3, 4, 5), 'Right', '3,4,5 is a Right triangle')
 
     def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        self.assertEqual(classifyTriangle(5, 3, 4), 'Right', '5,3,4 is a Right triangle')
         
     def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classifyTriangle(1, 1, 1), 'Equilateral', '1,1,1 should be equilateral')
+
+    # Bruno's additionally defined test cases:
+
+    def test_isosceles_A(self):
+        self.assertEqual(classifyTriangle(40, 40, 60), 'Isosceles', '40, 40, 60 should be isosceles')
+
+    def test_isosceles_B(self):
+        self.assertEqual(classifyTriangle(2, 7, 7), 'Isosceles', '2, 7, 7 should be isosceles')
+
+    def test_scalene_A(self):
+        self.assertEqual(classifyTriangle(1, 2, 3), 'Scalene', '1, 2, 3 should be scalene')
+
+    def test_scalene_B(self):
+        self.assertEqual(classifyTriangle(100, 99, 102), 'Scalene', '100, 99, 102 should be scalene')
+
+    def test_equilateral_C(self):
+        self.assertEqual(classifyTriangle(0.1, 0.1, 0.1), 'InvalidInput', '0.1, 0.1, 0.1 has non-integer sides, making the input invalid')
 
 if __name__ == '__main__':
     print('Running unit tests')
