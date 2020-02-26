2eb1d0b2edd04584223ce17bf5371f1c0de347fe
Bruno Salgado <bsalgado@stevens.edu>
2020-02-26 01:47:08 -0500

    Mocked out API requests for testing

--- HW04/test__main__.py
+++ HW04/test__main__.py
@@ -1,11 +1,24 @@
-'''
+"""
     Bruno Salgado, February 2020
     This script contains an incomplete pass of unit tests for the retrieve github functions
-'''
+"""
 
 import retrieve_github
+from unittest.mock import Mock, patch
+import json
 
-def test_retrieve_github():
-    user = retrieve_github.retrieve_user('bsalgado98')
-    assert  user != None
-    assert retrieve_github.retrieve_repos(user) != None
+
+@patch('retrieve_github.retrieve_user')
+def test_retrieve_github(mock_user_data):
+    with open('mock_user_data.json') as json_file:
+        mock_user_data = json.load(json_file)
+    assert mock_user_data is not None
+    assert retrieve_github.retrieve_repos(mock_user_data) is not None
+
+@patch('retrieve_github.retrieve_repo_commits')
+def test_retrieve_repo_commits(mock_repo_data):
+    with open('mock_repo_data.json') as json_file:
+        mock_repo_data = json.load(json_file)
+        mock_repo_data.github_username = "bsalgado98"
+    assert mock_repo_data is not None
+    assert retrieve_github.retrieve_repo_commits(mock_repo_data.github_username) is not None
